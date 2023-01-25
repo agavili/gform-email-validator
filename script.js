4e90d34a44adba7fe28700552982da81b2471dc8
agavili <60236229+agavili@users.noreply.github.com>
2023-01-25 11:49:37 -0500

    validate entries via button if email input exists

--- script.js
+++ script.js
@@ -30,52 +30,54 @@ function fadein(element) {
 
 async function ValidateEmailResponse() {
 
-    //AbstractAPI API Key 
-    YOUR_UNIQUE_API_KEY = 'your-api-key';
-
-    //Create Button
-    const form_bottom = document.querySelector('div[class="lRwqcd"]');
-    const validateButton = document.createElement("button");
-    validateButton.innerHTML = "Validate Entries";
-    validateButton.className = "validate-button";
-    validateButton.setAttribute("id", "button-validate");
-
-    //Style Button 
-    validateButton.style.backgroundColor = '#663ab7';
-    validateButton.style.fontFamily = 'Roboto, sans-serif';
-    validateButton.style.color='#faf8fd';
-    validateButton.style.borderRadius='6px';
-    validateButton.style.border='0';
-    validateButton.style.padding = '10px 20px 10px 20px';
-
-    //Add Button to Page
-    form_bottom.appendChild(validateButton);
-    
-    //Obtain Email Address
+    //Obtain Email Address Field 
     const email_element = document.querySelector('input[type="email"]');
-    
-    // Validation Logic 
+
+    //Check if Email Input Exists 
     if(email_element != null){
-        validateButton.addEventListener("click", function() {
-            email = email_element.value; 
-            const res = fetch(`https://emailvalidation.abstractapi.com/v1/?api_key=${YOUR_UNIQUE_API_KEY}&email=${email}`)
-            .then(function (response) {
-                console.log(response.clone().json())
-                return response.json();
-            }).then(function (data) {
-                var deliverability = data.deliverability;
-                if(deliverability != 'DELIVERABLE'){
-                    alert("The email you've entered is unreachable, please re-enter it 🙏");
-                }else{
-                    validateButton.innerHTML = "Entries are Valid 🙌";
-                    validateButton.style.backgroundColor = '#137E06';
-                    fadein(validateButton);
-                }
-            }).catch(function (err) {
-                console.warn('Something went wrong.', err);
+
+        //AbstractAPI API Key 
+        YOUR_UNIQUE_API_KEY = 'your-api-key';
+
+        //Create Button
+        const form_bottom = document.querySelector('div[class="lRwqcd"]');
+        const validateButton = document.createElement("button");
+        validateButton.innerHTML = "Validate Entries";
+        validateButton.className = "validate-button";
+        validateButton.setAttribute("id", "button-validate");
+
+        //Style Button 
+        validateButton.style.backgroundColor = '#663ab7';
+        validateButton.style.fontFamily = 'Roboto, sans-serif';
+        validateButton.style.color='#faf8fd';
+        validateButton.style.borderRadius='6px';
+        validateButton.style.border='0';
+        validateButton.style.padding = '10px 20px 10px 20px';
+
+        //Add Button to Page
+        form_bottom.appendChild(validateButton);
+        
+        // Validation Logic 
+            validateButton.addEventListener("click", function() {
+                email = email_element.value; 
+                const res = fetch(`https://emailvalidation.abstractapi.com/v1/?api_key=${YOUR_UNIQUE_API_KEY}&email=${email}`)
+                .then(function (response) {
+                    console.log(response.clone().json())
+                    return response.json();
+                }).then(function (data) {
+                    var deliverability = data.deliverability;
+                    if(deliverability != 'DELIVERABLE'){
+                        alert("The email you've entered is unreachable, please re-enter it 🙏");
+                    }else{
+                        validateButton.innerHTML = "Entries are Valid 🙌";
+                        validateButton.style.backgroundColor = '#137E06';
+                        fadein(validateButton);
+                    }
+                }).catch(function (err) {
+                    console.warn('Something went wrong.', err);
+                });
             });
-        });
-}
+    }
 
 }
 
